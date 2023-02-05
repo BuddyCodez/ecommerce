88e72b70f2dc14becc80e2bfabaed2a55cb2871b
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-02-05 15:57:04 +0530

    NEW

--- pages/watch.js
+++ pages/watch.js
@@ -8,10 +8,11 @@ const watch = () => {
   const [anime, setAnime] = useState([]);
   const [source, setSource] = useState([]);
   const [FilterdSource, setFilterdSource] = useState([]);
-  const [videoQuality, setVideoQuality] = useState("360p");
+  const [videoQuality, setVideoQuality] = useState("720p");
   useEffect(() => {
     const getAnime = async () => {
-      const url = "https://api.consumet.org/anime/gogoanime/info/" + query.anime;
+      const url =
+        "https://api.consumet.org/anime/gogoanime/info/" + query.anime;
       const { data } = await axios.get(url);
       setAnime(data);
       console.log(data);
@@ -20,6 +21,9 @@ const watch = () => {
       const url = "https://api.consumet.org/anime/gogoanime/watch/" + query.id;
       const { data } = await axios.get(url);
       setSource(data.sources);
+      setTimeout(() => {
+        setVideoQuality("360p");
+      }, 3000);
     };
     if (query.anime) {
       getAnime();
@@ -30,7 +34,8 @@ const watch = () => {
   }, [query]);
   useEffect(() => {
     const filter = async () => {
-      console.log(source);
+      console.log("source",source);
+      
       const filterd = source.filter((item) => {
         return item.quality === videoQuality;
       });
@@ -38,8 +43,7 @@ const watch = () => {
       setFilterdSource(filterd);
     };
     filter();
-    setVideoQuality("480p");
-    setVideoQuality("360p");
+
   }, [source, videoQuality]);
   useEffect(() => {
     const submenu = document.querySelector('vm-submenu[label="Quality"]');
@@ -64,14 +68,15 @@ const watch = () => {
                 {anime?.animeTitle}
               </h1>
               <div className="flex flex-col justify-center items-center gap-2">
-                <vm-player contorls class="player">
-                  <vm-hls cross-origin="true" poster={anime?.image}>
+                <vm-player contorls class="player" autoplay>
+                  <vm-hls crossOrigin="true" poster={anime?.image}>
                     <source
                       data-src={FilterdSource[0]?.url}
                       type="application/x-mpegURL"
                     />
                   </vm-hls>
                   <vm-default-ui no-click-to-play no-settings>
+                    <vm-spinner></vm-spinner>
                     <vm-settings>
                       <vm-submenu label="Quality" hint="360p">
                         <vm-menu-radio-group value="1">
@@ -93,6 +98,9 @@ const watch = () => {
                           ></vm-menu-radio>
                         </vm-menu-radio-group>
                       </vm-submenu>
+                      <vm-menu-item label="Forward" hint="10s"></vm-menu-item>
+                      <vm-menu-item label="Backward" hint="10s"></vm-menu-item>
+                      <vm-menu-item label="Next Episode" hint="next"></vm-menu-item>
                     </vm-settings>
                   </vm-default-ui>
                 </vm-player>
@@ -106,7 +114,7 @@ const watch = () => {
               <h2 className="h2">Select Episode: {anime?.totalEpisodes}</h2>
               <ul className="Episodes">
                 {anime?.episodes?.map((episode) => {
-                  if (episode.id === query.id) return;
+                  if (episode.id > query.id) return;
                   return (
                     <li key={episode.id}>
                       <Link
