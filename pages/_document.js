5e8561baf5381d339c7043de37ae8f0c404a8fa8
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-02-10 14:18:11 +0530

    new push

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,43 @@ export default function Document() {
     <Html lang="en">
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="description" content="Anime has always been a source of entertainment for people of all ages and interests. With a vast collection of unique and captivating stories, anime has become one of the most popular forms of entertainment worldwide. At Animatrix, we are dedicated to providing the best anime viewing experience to our users. Our website offers a wide variety of anime shows and movies, ranging from classic to the latest releases.
 
+With a user-friendly interface and high-quality video streaming, we ensure that our users can enjoy their favorite anime shows without any interruptions. Our website is optimized for both desktop and mobile devices, making it easy for users to access it from anywhere, at any time.
+
+Our team is constantly updating our anime library to ensure that our users have access to the latest and greatest anime shows. We also have a community of anime enthusiasts who discuss and share their thoughts on various anime shows, making our website a hub for all things anime.
+
+At Animatrix, we believe in providing the best possible experience to our users, which is why we are dedicated to delivering high-quality anime content, fast and reliable streaming, and an active community. So why wait? Start watching your favorite anime shows today and join the Animatrix community!" />
+      <meta name="keywords" content="Anime,
+Japanese animation,
+Cartoons,
+Animated series,
+Streaming,
+Online anime,
+Watch anime,
+Best anime,
+Latest anime,
+Classic anime
+Popular anime,
+Anime shows,
+Anime movies,
+Anime episodes,
+Anime community,
+Anime discussion,
+Anime news,
+Anime merchandise,
+High-quality anime,
+User-friendly anime website" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+      <meta property="og:title" content="Animatrix" />
+      <meta property="og:image" content="/images/favlogo.png" />
+      <meta property="og:url" content="https://animematrix.vercel.app/" />
       <title>AniMatrix - All in one Anime Collection</title>
-
+      <meta name="google-site-verification" content="d8Q9bSGyoBL8RSathwiLAJd3qbQhUcl_au7udJd5XZo" />
       <link
         rel="shortcut icon"
-        href="/images/favicon.svg"
+        href="/images/favlogo.png"
         type="image/svg+xml"
       />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
@@ -48,7 +79,7 @@ export default function Document() {
           type="module"
           src="https://cdn.jsdelivr.net/npm/@vime/core@^5/dist/vime/vime.esm.js"
         ></script>
- 
+
       </body>
     </Html>
   );
