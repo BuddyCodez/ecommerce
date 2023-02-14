c88004b85d3f917ac0b76483b12f12bf535b8328
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-02-14 11:51:42 +0530

    new update ig

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,33 @@ import '@/styles/global.css';
 import '@/styles/others.css';
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
+import { DefaultSeo, LogoJsonLd } from 'next-seo';
 
 export default function App({ Component, pageProps }) {
   return <>
+    <LogoJsonLd url='https://animematrix.vercel.app/' logo='https://animematrix.vercel.app/images/BrandedLogo.png' />
+    <DefaultSeo
+      title='Animeterra - All in one anime platform'
+      description='Animeterra is a platform where you can watch anime, read manga, and read light novels.'
+      canonical='https://animematrix.vercel.app/'
+      openGraph={{
+        title: 'Animeterra - All in one anime platform',
+        description: 'Animeterra is a platform where you can watch anime, read manga, and read light novels.',
+        images: [
+          {
+            url: 'https://animematrix.vercel.app/images/BrandedLogo.png',
+            alt: 'Animeterra'
+          }
+        ],
+
+        type: 'article',
+        article: {
+          tags: ['Animeterra', 'animeterra', 'ANIMETERRA', 'anime', 'manga', 'light novel', 'lightnovel', 'light novels', 'lightnovels', 'anime platform', 'anime platform']
+        },
+        url: 'https://animematrix.vercel.app/',
+        site_name: 'Animeterra - All in one anime platform',
+
+      }} />
     <Component {...pageProps} />
     <Analytics />
     <Script src='/js/script.js' />
--- pages/_document.js
+++ pages/_document.js
@@ -5,25 +5,9 @@ export default function Document() {
     <Html lang="en">
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="description" content="Anime has always been a source of entertainment for people of all ages and interests. With a vast collection of unique and captivating stories, anime has become one of the most popular forms of entertainment worldwide. At Animeterra, we are dedicated to providing the best anime viewing experience to our users. Our website offers a wide variety of anime shows and movies, ranging from classic to the latest releases.
-
-With a user-friendly interface and high-quality video streaming, we ensure that our users can enjoy their favorite anime shows without any interruptions. Our website is optimized for both desktop and mobile devices, making it easy for users to access it from anywhere, at any time.
-
-Our team is constantly updating our anime library to ensure that our users have access to the latest and greatest anime shows. We also have a community of anime enthusiasts who discuss and share their thoughts on various anime shows, making our website a hub for all things anime.
-
-At Animeterra, we believe in providing the best possible experience to our users, which is why we are dedicated to delivering high-quality anime content, fast and reliable streaming, and an active community. So why wait? Start watching your favorite anime shows today and join the Animeterra community!" />
-      <meta name="keywords" content=" Animeterra, animeterra, ANIMETERRA, Anime, Japanese animation, Cartoons, Animated series
-" />
-      <meta property="og:title" content="Animeterra" />
-      <meta property="og:image" content="/images/favlogo.png" />
-      <meta property="og:url" content="https://animematrix.vercel.app/" />
-      <title>Animeterra - All in one Anime Collection</title>
+     
       <meta name="google-site-verification" content="d8Q9bSGyoBL8RSathwiLAJd3qbQhUcl_au7udJd5XZo" />
-      <link
-        rel="shortcut icon"
-        href="/images/favlogo.png"
-        type="image/svg+xml"
-      />
+      
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
